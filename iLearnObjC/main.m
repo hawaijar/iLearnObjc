ea416d40fb9a624144729851450374015589799c
hawaijar <hawaijar.js@gmail.com>
2021-10-11 09:15:58 +0530

    Initial Commit

---
+++ iLearnObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iLearnObjC
+//
+//  Created by Sushil Mayengbam on 11/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
